4b8a2738dd956cdbc40d03fbea282c435a669af7
Ivan Patskan <hosp54@DESKTOP-1CFD7GK.>
2024-10-13 15:25:26 +0700

    Add new sequence_helicopter script wersion with fastq_rader

--- sequence_helicopter.py
+++ sequence_helicopter.py
@@ -11,26 +11,29 @@ from gc_and_quality_calculator import (
     calculate_quality
 )
 
+from fastq_reader import (fastq_to_dict, write_fastq)
 
-def filter_fastq(seqs: dict[str, tuple[str, str]],
+
+
+def filter_fastq(input_fastq: str, output_fastq: str,
                  gc_bounds: tuple[float, float] = (0, 100),
                  length_bounds: tuple[int, int] = (0, 2**32),
-                 quality_threshold: float = 0) -> dict[str, tuple[str, str]]:
-    
+                 quality_threshold: float = 0) -> None:
     """
-    Фильтрует FASTQ-последовательности по параметрам GC-состава, длины и порогу качества.
+    Filters sequences from an input FASTQ file based on GC content, length, and quality threshold
+    and writes the filtered sequences to an output FASTQ file.
 
-    Аргументы:
-    seqs -- словарь, где ключи это имена последовательностей (str),
-    а значения кортежи с двумя строками: (последовательность, качество).
-    gc_bounds -- кортеж с минимальным и максимальным значениями для GC-состава.
-    length_bounds -- кортеж с минимальной и максимальной длиной последовательности.
-    quality_threshold -- порог среднего качества последовательности.
+    Arguments:
+    input_fastq -- the path to the input FASTQ file (str).
+    output_fastq -- the path to the output FASTQ file (str).
+    gc_bounds -- a tuple with the minimum and maximum GC content values (default is (0, 100)).
+    length_bounds -- a tuple with the minimum and maximum sequence length (default is (0, 2**32)).
+    quality_threshold -- the threshold for the average quality score (default is 0).
 
-    Возвращает:
-    Словарь с отфильтрованными последовательностями, которые соответствуют условиям фильтрации.
+    Returns:
+    None
     """
-
+    seqs = fastq_to_dict(input_fastq)
     good_filtered_seqs = {}  # Пустой словарь для запист отфильтрованных посл.
 
     # Цикл, который будет вынимать из словаря каждую пару ключ-значение
@@ -39,7 +42,7 @@ def filter_fastq(seqs: dict[str, tuple[str, str]],
     # key in seqs - строка с имем последовательнсти
     # value in seqs - кортеж с двумя строками - (последовательность, качество)
 
-    for seq_name, (seq_str, quality_str) in seqs.items():
+    for seq_name, (seq_str, quality_str, plus_str) in seqs.items():
         gc_content = calculate_gc_content(seq_str)  # Считаем GC-состав
         avg_quality = calculate_quality(quality_str)  # Считаем ср. качество
         length_seq = len(seq_str)                   # Длина последовательности
@@ -76,10 +79,20 @@ def filter_fastq(seqs: dict[str, tuple[str, str]],
 
         if gc_status and length_status and quality_status:
             # Записываем элемент словаря seqs через ключ в новый словарь
-            good_filtered_seqs[seq_name] = (seq_str, quality_str)
+            good_filtered_seqs[seq_name] = (seq_str, quality_str, plus_str)
 
-    return good_filtered_seqs
 
+
+        # Фильтрация данных
+    good_filtered_seqs = filter_fastq(seqs)
+
+    if not good_filtered_seqs:
+        print("No sequences match the filtering criteria.")
+    else:
+        print(f"Sequences filtered: {len(good_filtered_seqs)}")
+
+    # Шаг 3: Запись отфильтрованных последовательностей в файл
+    write_fastq(good_filtered_seqs, output_fastq)
 
 def run_dna_rna_tools(*args: str) -> list[str]:
 
@@ -102,3 +115,4 @@ def run_dna_rna_tools(*args: str) -> list[str]:
         return complement(seqs)
     elif command == "reverse_complement":
         return reverse_complement(seqs)
+
